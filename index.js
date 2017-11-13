7853edbf273ccac752d838af6691afa54979a17b
AnthonyCatalfo <anthonycatalfo@gmail.com>
2017-11-13 02:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Write your solution in this file!
- var customerName='bob';
+var customerName='bob';
 
 function upperCaseCustomerName() {
-customerName=customerName.toUpperCase()
-return
+customerName=customerName.toUpperCase();
+return;
 }
 function setBestCustomer() {
-  bestCustomer= "not bob"
-  return
-  }
-  function overwriteBestCustomer() {
-  bestCustomer= "BigT"
-  return
-  }
-  const
+ bestCustomer= "not bob";
+ return;
+ }
+ function overwriteBestCustomer() {
+ bestCustomer= "BigT";
+ return;
+ }
